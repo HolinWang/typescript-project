79235e4ccceb41442c682646dbda3ecbdc2ab01c
Holin <Holin.wang@outlook.com>
2023-04-23 00:30:32 +0800

    安装ts-node 执行指令 ts-node .\demo.ts

---
+++ src/demo.ts
@@ -0,0 +1,12 @@
+/**
+* 模块名:
+* 代码描述: 安装ts-node 执行指令 ts-node .\demo.ts
+* 作者: Holin  Wang
+* 创建时间:2023/04/23 00:26:32
+*/
+
+function holin(){
+  let web: string = "hello Holin";
+  console.log(web)
+}
+holin()
